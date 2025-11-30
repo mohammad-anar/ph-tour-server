6311ff579d9c4cfc0f7c86529cc3738d809c1740
mohammad-anar <ai.anarul4232@gmail.com>
2025-11-30 07:03:30 +0600

    payment stats added

--- src/app/modules/stats/stats.service.ts
+++ src/app/modules/stats/stats.service.ts
@@ -1,4 +1,6 @@
 import { Booking } from "../booking/booking.model";
+import { PAYMENT_STATUS } from "../payment/payment.interface";
+import { Payment } from "../payment/payment.model";
 import { Tour } from "../tour/tour.model";
 import { IsActive, IUser } from "../user/user.interfaces";
 import { User } from "../user/user.model";
@@ -298,7 +300,70 @@ const getBookingStats = async () => {
 };
 
 const getPaymentStats = async () => {
-  return null;
+  const totalPaymentPromise = Payment.countDocuments();
+
+  const totalPaymentsByStatusPromise = Payment.aggregate([
+    // stage 1
+    {
+      $group: {
+        _id: "$status",
+        count: { $sum: 1 },
+      },
+    },
+  ]);
+
+  const totalRevenuePromise = Payment.aggregate([
+    // stage 1
+    {
+      $match: { status: PAYMENT_STATUS.PAID },
+    },
+    {
+      $group: {
+        _id: null,
+        totalRevenue: { $sum: "$amount" },
+      },
+    },
+  ]);
+
+  const avgPaymentAmountPromise = Payment.aggregate([
+    {
+      $group: {
+        _id: null,
+        avgPayment: { $avg: "$amount" },
+      },
+    },
+  ]);
+
+  const paymentGatewayDataPromise = Payment.aggregate([
+    {
+      $group: {
+        _id: { $ifNull: ["$paymentGatewayData.status", "UNKNOWN"] },
+        count: { $sum: 1 },
+      },
+    },
+  ]);
+
+  const [
+    totalPayment,
+    totalPaymentsByStatus,
+    totalRevenue,
+    avgPaymentAmount,
+    paymentGatewayData,
+  ] = await Promise.all([
+    totalPaymentPromise,
+    totalPaymentsByStatusPromise,
+    totalRevenuePromise,
+    avgPaymentAmountPromise,
+    paymentGatewayDataPromise,
+  ]);
+
+  return {
+    totalPayment,
+    totalPaymentsByStatus,
+    totalRevenue,
+    avgPaymentAmount,
+    paymentGatewayData,
+  };
 };
 
 export const StatsService = {
